c5e09794b8343014ba79414a461ec3c223953e1d
Adithya Nair <adithyadoha123@gmail.com>
2024-07-01 00:22:25 +0530

    didnt need that

--- Common_Tasks/Javascript/app.js
+++ Common_Tasks/Javascript/app.js
@@ -1,5 +1,4 @@
 let boxes = document.querySelectorAll(".box");
-let resetBtn = document.querySelector("#reset-btn");
 let msgContainer = document.querySelector(".msg-container");
 let msg = document.querySelector("#msg");
 
